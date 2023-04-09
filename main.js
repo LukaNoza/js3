b0bc8ffb3d6905603d27688f28e1562b23c506a6
Luka Noza <luka.nozadze25b@geolab.edu.ge>
2023-04-09 16:13:49 +0400

    de

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-function sortArray(arr) {
-  var n = arr.length;
-  for (var i = 0; i < n; i++) {
-    for (var j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j+1]) {
-        var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+function sortArray() {
+    var numbers = [];
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var num3 = parseInt(document.getElementById("num3").value);
+    var num4 = parseInt(document.getElementById("num4").value);
+    var num5 = parseInt(document.getElementById("num5").value);
+  
+    numbers.push(num1, num2, num3, num4, num5);
+  
+    var n = numbers.length;
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n - i - 1; j++) {
+        if (numbers[j] > numbers[j+1]) {
+          var temp = numbers[j];
+          numbers[j] = numbers[j+1];
+          numbers[j+1] = temp;
+        }
       }
     }
+  
+    document.getElementById("originalArray").innerHTML = numbers.join(', ');
+    document.getElementById("sortedArray").innerHTML = numbers.join(', ');
   }
-}
-
-var numbers = [];
-for (var i = 0; i < 5; i++) {
-  var num = parseInt(prompt("Enter number " + (i+1) + ": "));
-  numbers.push(num);
-}
-
-console.log("Original array:", numbers);
-sortArray(numbers);
-console.log("Sorted array:", numbers);
+  
